fb693161da6bab9c2cbca66ba014b718ad802eee
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2019-01-07 23:58:43 -0500

    Update gradient.js

--- gradient.js
+++ gradient.js
@@ -1,5 +1,11 @@
 export class Gradient (
     constructor() {
-        //
+        this.width = 500;
+        this.height = 100;
+    }
+    events() {
+        document.body.addEventListener("mousemove", (e) => {
+
+        });
     }
 );
