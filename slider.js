82ee3da8b7ce8f56bfd85009279c0a2802d8078f
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2019-01-07 23:58:45 -0500

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,3 +1,5 @@
+import { absolute } from "./common-styles.js";
+
 export let HORIZONTAL = 0;
 export let VERTICAL = 1;
 
@@ -15,15 +17,29 @@ export class Slider {
 
   constructor(view, target, specimen_id, id, name, array_xy_pos, direction,   width, height,     min, max, now,   show_value) {
 
-    this.container = document.getElementById( target );
-    this.specimen = null; // document.getElementById( specimen_id );
-    this.specimen_corners = document.getElementById( "specimen_corners" );
-    this.direction = direction;
-    this.value_width = 60;
-    this.mouse = view.mouse;
+    // the view
     this.view = view;
 
-    // flip axis
+    // grab globally available mouse object
+    this.mouse = view.mouse;
+
+    // get element object of the container it will be embedded into
+    this.container = document.getElementById( target );
+
+    // store a reference to the object the values on this slider control will modify
+    this.specimen = null;
+
+    // The gradient preview in the main dashboard
+    this.specimen_corners = document.getElementById( "specimen_corners" );
+
+    this.direction = direction;
+
+    // width of the box where numeric value of this
+    // slider is displayed (to the right of actual slider)
+    // *not used if "show_value" argument is false or null
+    this.value_width = 60;
+
+    // Slider can be horizontal or vertical, horizontal is default, but flip axis if its vertical
     if (this.direction == VERTICAL) { let save = width; width = height; height = save; }
 
     this.width = width;
@@ -48,73 +64,76 @@ export class Slider {
 
     // Main slider area
     this.div = document.createElement("div");
-    this.div.setAttribute("id", id);
+
+    // Create slider container
+    this.div = absolute(id,
+                        array_xy_pos == null ? 0 : array_xy_pos[0],
+                        array_xy_pos == null ? 0 : array_xy_pos[1],
+                        this.width,
+                        this.height,
+                        1);
     this.div.style.marginTop = "8px";
-
-    if (array_xy_pos == null) {
-      this.div.style.position = "relative";
-    } else {
-      this.div.style.position = "absolute";
-      this.div.style.left = array_xy_pos[0] + "px";
-      this.div.style.top = array_xy_pos[1] + "px";
-    }
-
+    // if position is not provided, make this element "relative" (position works only with "absolute" display location)
+    if (array_xy_pos == null) this.div.style.position = "relative";
     this.div.setAttribute("class", "noselect");
-    this.div.style.display = "block";
-    this.div.style.width = this.width + "px";
-    this.div.style.height = this.height + "px";
     this.div.style.background = this.background.color;
     this.div.style.color = "gray";
+    // Print name of this control inside it
     this.div.innerHTML = "<span style = 'padding-left: 8px; width: 100%; display: inline-block; text-align: left;'>" + name + "</span>";
+    // Determine color and font of the name displayed inside the element
     this.div.style.fontFamily = "Arial";
     this.div.style.fontSize = "14px";
     this.div.style.color = "white";
+    // Match line height to the height of the element
     this.div.style.lineHeight = this.height + "px";
-    this.div.style.zIndex = 1;
-    //this.div.style.background = "conic-gradient(red, yellow, lime, aqua, transparent, magenta, red)";
-    this.div.style.overflow = "hidden";
+    // Add overflow hidden
+    //this.div.style.overflow = "hidden";
 
-    // Draggable scrubber
-    this.knob = document.createElement("div");
-    this.knob.setAttribute("id", id + "_knob");
+    // Create the draggable knob
+    this.knob =  absolute(id + "_knob",
+                          0,
+                          this.scrubber.x,
+                          this.scrubber.width,
+                          this.scrubber.height,
+                          12);
     this.knob.setAttribute("class", "scrubber");
-    this.knob.style.position = "absolute";
-    this.knob.style.display = "block";
-    this.knob.style.left = this.scrubber.x + "px";
-    this.knob.style.top = 0 + "px";
-    this.knob.style.width = this.scrubber.width + "px";
-    this.knob.style.height = this.scrubber.height + "px";
     this.knob.style.background = this.scrubber.color;
     this.knob.style.cursor = "hand";
-    this.knob.style.zIndex = 2;
 
-    // value display
+    // Create value display <div> (if enabled)
     if (show_value) {
-      this.val = document.createElement("input");
-      this.val.setAttribute("id", id + "_value");
-      this.val.style.position = "absolute";
-      this.val.style.top = 0;
-      this.val.style.right = -this.value_width + "px";
-      this.val.style.display = "block";
-      this.val.style.width = this.value_width + "px";
-      this.val.style.height = this.height + "px";
-      this.val.style.background = "white";
-      this.val.style.border = "1px solid gray";
-      this.val.style.color = "#333";
-      this.val.style.fontFamily = "Arial";
-      this.val.style.fontSize = "12px";
-      this.val.value = this.now;
-      this.val.style.zIndex = 3;
+         this.val =  absolute(id + "_value",
+                              0,
+                              0,
+                              this.value_width,
+                              this.height,
+                              13,
+                              -this.value_width); // align to right side
+         this.val.style.background = "white";
+         this.val.style.border = "1px solid gray";
+         this.val.style.color = "#333";
+         this.val.style.fontFamily = "Arial";
+         this.val.style.fontSize = "12px";
+         this.val.innerHTML = "0";
+         this.val.value = this.now;
     }
 
-    if (this.val) this.div.appendChild( this.val );
+    if (this.val) {
+        // console.log("Nest value inside Slider container")
+        this.div.appendChild( this.val );
+    }
+
+    // Nest the knob inside the container
     this.div.appendChild( this.knob );
+
+    // Add the slider to target in the DOM
     this.container.appendChild( this.div );
 
-    this.enable_drag();
+    // Add mouse drag events
+    this.events();
   }
 
-  enable_drag() {
+  events() {
 
     // knob
     this.knob.addEventListener("mousedown", e => {
@@ -134,14 +153,21 @@ export class Slider {
 
     // main events
     document.body.addEventListener("mousemove", e => {
+
       if (this.isDragging == true) {
+
+        // Current value of this slider
         let val, bound;
+
+        // Horizontal
         if (this.direction == HORIZONTAL) {
           val = this.memory.x + this.mouse.difference.x;
           bound = this.width - this.scrubber.width;
           if (val < 0) val = 0;
           if (val > bound) val = bound;
           this.knob.style.left = val + "px";
+
+        // Must be vertical
         } else {
           val = this.memory.y + this.mouse.difference.y;
           bound = this.height - this.scrubber.height;
@@ -150,16 +176,21 @@ export class Slider {
           this.knob.style.top = val + "px";
         }
 
+        // Keep lime color if mouse is outside of the knob but still dragging
         this.knob.style.background = "lime";
 
-        if (this.val) this.val.value = val;
+        // Update new slider value in slider value display <div>
+        if (this.val) this.val.innerHTML = val;
+
+        // Do something to the currently selected "specimen" element,
+        // That this slider is supposed to change. This could be a color,
+        // The size (width & height) or rotation angle of target element.
 
         this.specimen = this.view.selectedElement; // this.view.selectedElement;
 
         // view zoom
-        if (this.name == "zoom") {
+        if (this.name == "zoom")
             document.getElementById("View").style.transform = "scale(" + (1.0 + (val * 0.001)) + ")";
-        }
 
         if (this.specimen) {
 
@@ -187,6 +218,8 @@ export class Slider {
             this.specimen_corners.style.borderBottomLeftRadius = this.specimen.style.borderBottomLeftRadius = orc[4] + " " + orc[5];
             this.specimen_corners.style.borderBottomRightRadius = this.specimen.style.borderBottomRightRadius = orc[6] + " " + orc[7];
         }
+
+
       }
     });
     document.body.addEventListener("mouseup", e => {
